02aad277f782d81a081e8f088cda964747472b54
Happy <hgp9825@gmail.com>
2023-03-19 12:43:11 +0530

    Video 61: iNotebook: Adding noteItem in a seprate note component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,7 @@
-import React, { useContext } from 'react'
-import noteContext from "../context/notes/noteContext"
+import Notes from './Notes';
 
 export const Home = () => {
-  const context = useContext(noteContext);
-  //Destructering 
-  const{notes, setNotes} = context;
+ 
   return (
     <div>
       <div className="container my-3">
@@ -27,12 +24,7 @@ export const Home = () => {
         </form>
       </div>
 
-      <div className="container my-3">
-        <h2>Your Notes</h2>
-        {notes.map((note)=>{
-          return note.title;
-        })}
-      </div>
+     <Notes/>
     </div>
   )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -13,6 +13,51 @@ const NoteState = (props) =>{
           "__v": 0
         },
         {
+          "_id": "641402ba8cb30eccbacc0cb1",
+          "user": "6412df6b37510cde83768d63",
+          "title": "My title",
+          "description": "Please wake up early",
+          "tag": "personal",
+          "date": "2023-03-17T06:03:38.182Z",
+          "__v": 0
+        },
+        {
+          "_id": "641402ba8cb30eccbacc0cb1",
+          "user": "6412df6b37510cde83768d63",
+          "title": "My title",
+          "description": "Please wake up early",
+          "tag": "personal",
+          "date": "2023-03-17T06:03:38.182Z",
+          "__v": 0
+        },
+        {
+          "_id": "641402ba8cb30eccbacc0cb1",
+          "user": "6412df6b37510cde83768d63",
+          "title": "My title",
+          "description": "Please wake up early",
+          "tag": "personal",
+          "date": "2023-03-17T06:03:38.182Z",
+          "__v": 0
+        },
+        {
+          "_id": "641402ba8cb30eccbacc0cb1",
+          "user": "6412df6b37510cde83768d63",
+          "title": "My title",
+          "description": "Please wake up early",
+          "tag": "personal",
+          "date": "2023-03-17T06:03:38.182Z",
+          "__v": 0
+        },
+        {
+          "_id": "641402ba8cb30eccbacc0cb1",
+          "user": "6412df6b37510cde83768d63",
+          "title": "My title",
+          "description": "Please wake up early",
+          "tag": "personal",
+          "date": "2023-03-17T06:03:38.182Z",
+          "__v": 0
+        },
+        {
           "_id": "641402dc8cb30eccbacc0cb3",
           "user": "6412df6b37510cde83768d63",
           "title": "My title",
