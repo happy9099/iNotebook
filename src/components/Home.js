1a3da1632de177c9d5a063d5a91b9b46869b90df
Happy <hgp9825@gmail.com>
2023-03-19 11:39:16 +0530

    Video 60: iNotebook : Fetching notes from Context

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import noteContext from "../context/notes/noteContext"
 
 export const Home = () => {
+  const context = useContext(noteContext);
+  //Destructering 
+  const{notes, setNotes} = context;
   return (
     <div>
       <div className="container my-3">
@@ -25,6 +29,9 @@ export const Home = () => {
 
       <div className="container my-3">
         <h2>Your Notes</h2>
+        {notes.map((note)=>{
+          return note.title;
+        })}
       </div>
     </div>
   )
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,10 +2,30 @@ import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) =>{
-    
+    const notesInitial =[
+        {
+          "_id": "641402ba8cb30eccbacc0cb1",
+          "user": "6412df6b37510cde83768d63",
+          "title": "My title",
+          "description": "Please wake up early",
+          "tag": "personal",
+          "date": "2023-03-17T06:03:38.182Z",
+          "__v": 0
+        },
+        {
+          "_id": "641402dc8cb30eccbacc0cb3",
+          "user": "6412df6b37510cde83768d63",
+          "title": "My title",
+          "description": "Please wake up early",
+          "tag": "personal",
+          "date": "2023-03-17T06:04:12.372Z",
+          "__v": 0
+        }
+      ]
+      const [notes, setNotes] = useState(notesInitial)
 
     return(
-        <NoteContext.Provider value={{}}>
+        <NoteContext.Provider value={{notes, setNotes}}>
             {props.children}
         </NoteContext.Provider>
     )
